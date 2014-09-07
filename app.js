a8e1be035b9f609db691e062ff4da2fd015c23ba
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-09-07 23:29:58 +0900

    toggle img

--- app.js
+++ app.js
@@ -4,16 +4,30 @@
     window.app = new Vue({
         el: '.items',
         data: {
+            items: [{
+                col: "col-xs-6",
+                img: "http://stat.ameba.jp/user_images/20131206/22/okayu1206/bf/96/j/o0480057612772498518.jpg",
+                done: false,
+            },
+            {
+                col: "col-xs-6",
+                img: "http://stat.ameba.jp/user_images/20131206/22/okayu1206/bf/96/j/o0480057612772498518.jpg",
+                done: false,
+            }],
         },
         ready: function () {
         },
         methods: {
-            toggleItem: function (e) {
-                $(e.target).parent()
-                    .removeClass("col-xs-6")
-                    .addClass("col-xs-12");
-                
+            toggleItem: function (item) {
+                item.done = !item.done;
 
+                // toggle
+                if (item.done) {
+                    item.col = "col-xs-12";
+                    console.log(item);
+                } else {
+                    item.col = "col-xs-6";
+                }
             },
         },
     });
