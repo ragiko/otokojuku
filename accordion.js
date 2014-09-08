431b3ee8fc06b2c33db8acddb767b357fd9ae762
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-09-08 22:19:28 +0900

    追加 accodion

--- accordion.js
+++ accordion.js
@@ -1,5 +1,10 @@
 (function () {
-    alert(0);
-
-
+    $(".item").on("click", function () {
+        $(".accodion").remove();
+        if ($(this).attr("id").slice(-1) % 2 == 0) {
+            $(this).parent().after("<div class='accodion right-item balloon-top'>Hello</div>");
+        } else {
+            $(this).parent().after("<div class='accodion left-item balloon-top'>Hello</div>");
+        }
+    });
 })();
