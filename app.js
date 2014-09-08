47ec1615ac0679b9bba06b86a76bff82b3c7f74d
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-09-08 16:11:01 +0900

    all UI complete

--- app.js
+++ app.js
@@ -4,30 +4,11 @@
     window.app = new Vue({
         el: '.items',
         data: {
-            items: [{
-                col: "col-xs-6",
-                img: "http://stat.ameba.jp/user_images/20131206/22/okayu1206/bf/96/j/o0480057612772498518.jpg",
-                done: false,
-            },
-            {
-                col: "col-xs-6",
-                img: "http://stat.ameba.jp/user_images/20131206/22/okayu1206/bf/96/j/o0480057612772498518.jpg",
-                done: false,
-            }],
         },
         ready: function () {
         },
         methods: {
-            toggleItem: function (item) {
-                item.done = !item.done;
-
-                // toggle
-                if (item.done) {
-                    item.col = "col-xs-12";
-                    console.log(item);
-                } else {
-                    item.col = "col-xs-6";
-                }
+            a: function () {
             },
         },
     });
