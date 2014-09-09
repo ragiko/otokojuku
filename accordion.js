c7ac62f7881d9300389053165c7025a933808f84
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-09-09 10:32:38 +0900

    add open & close html

--- accordion.js
+++ accordion.js
@@ -1,10 +1,16 @@
 (function () {
     $(".item").on("click", function () {
-        $(".accodion").remove();
+        var $accordion = $(".accordion-template");
+
+        $accordion.removeClass("accordion-template");
+        $(".accordion").remove();
+
         if ($(this).attr("id").slice(-1) % 2 == 0) {
-            $(this).parent().after("<div class='accodion right-item balloon-top'>Hello</div>");
+            // $(this).parent().after("<div class='accodion right-item balloon-top'>Hello</div>");
+            $accordion.addClass("right-item")
+            $(this).parent().after($accordion);
         } else {
-            $(this).parent().after("<div class='accodion left-item balloon-top'>Hello</div>");
+            // $(this).parent().after("<div class='accodion left-item balloon-top'>Hello</div>");
         }
     });
 })();
